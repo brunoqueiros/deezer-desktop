d0f032c4eeb9bdac06aa4df429362f62556c57a7
Bruno Queiros <brunoofq@gmail.com>
2015-11-01 01:47:21 -0200

    let some variables private

--- src/player-helper.js
+++ src/player-helper.js
@@ -1,39 +1,43 @@
 'use strict';
 
+var doc = null;
+var elements = {};
+
 class PlayerHelper {
   constructor() {
-    this.document = document.getElementById('iframe').contentWindow.document;
-    this.setElements();
-  }
-
-  setElements() {
-    this.elements = {
-      'play': this.document.querySelector('.control-play'),
-      'pause': this.document.querySelector('.control-pause'),
-      'repeat': this.document.querySelector('.control-repeat'),
-      'trackArtist': this.document.querySelector('.player-track-link'),
-      'trackName': this.document.querySelector('.player-track-link')
-    }
+    doc = document.getElementById('iframe').contentWindow.document;
+    elements = {
+      'play': doc.querySelector('.control-play'),
+      'pause': doc.querySelector('.control-pause'),
+      'next': doc.querySelector('.control-next'),
+      'repeat': doc.querySelector('.control-repeat'),
+      'trackArtist': doc.querySelector('.player-track-link'),
+      'trackName': doc.querySelector('.player-track-link')
+    };
   }
 
   playPause() {
-    if (this.elements['play']) {
-      this.elements['play'].click();
+    if (elements['play']) {
+      elements['play'].click();
     } else {
-      this.elements['pause'].click();
+      elements['pause'].click();
     }
   }
 
   repeat() {
-    if (this.elements['repeat'].classList.contains('active')) {
-      this.elements['repeat'].classList.remove('active');
+    if (elements['repeat'].classList.contains('active')) {
+      elements['repeat'].classList.remove('active');
     } else {
-      this.elements['repeat'].classList.add('active');
+      elements['repeat'].classList.add('active');
     }
   }
 
+  nextTrack() {
+    elements['next'].click();
+  }
+
   getCurrentMusic() {
-    return this.elements['trackArtist'] + ' ' + this.elements['trackName'];
+    return elements['trackArtist'] + ' ' + elements['trackName'];
   }
 }
 
