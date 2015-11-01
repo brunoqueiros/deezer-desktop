4d93f553e436419713cfc8a9a645a959de3c0135
Bruno Queiros <brunoofq@gmail.com>
2015-10-31 22:25:40 -0200

    create shortcuts file

---
+++ src/shortcuts.js
@@ -0,0 +1,42 @@
+'use strict';
+
+const globalShortcut = require('global-shortcut');
+const path = require('path');
+const Logger = require('./logger');
+
+let shortcutsList = [];
+
+class Shortcuts {
+  register(options) {
+    if (!options.key) {
+      throw 'Must be inform a shortcut key';
+    }
+
+    if (!shortcutsList[options.key]) {
+      shortcutsList[options.key] = options.actions;
+      Logger.info('shortcut registered ' + options.accelerator);
+
+      if (options.accelerator) {
+        const shortcut = globalShortcut.register(options.accelerator, options.action);
+
+        if (!shortcut) {
+          Logger.warn('registration failed ' + options.accelerator);
+        }
+      }
+    } else {
+      Logger.error('Already have a shortcut key ' + options.key);
+    }
+  }
+
+  send(key) {
+    if (shortcutsList[key]) {
+      shortcutsList[key]();
+    }
+  }
+
+  getAll() {
+    console.log(shortcutsList);
+  }
+}
+
+module.exports = new Shortcuts();
