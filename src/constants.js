732984833f5c4c8af7ed7aa459bef39da6def5eb
Bruno Queiros <brunoofq@gmail.com>
2015-11-01 18:32:05 -0200

    add PLAYER_IMG_URI and APP_TEMP_FOLDER to constants file

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,4 @@
+
 'use strict';
 var path = require('path');
 
@@ -6,12 +7,15 @@ const constants = {
   APP_WIDTH: 1000,
   APP_HEIGHT: 650,
   APP_LOGO: path.join(__dirname, '../assets', 'logo.png'),
+  APP_TMP_FOLDER: path.join(__dirname, '../.tmp'),
 
   PLAY_PAUSE: 'play-pause',
   NEXT: 'next',
   PREV: 'prev',
   SHUFFLE: 'shuffle',
   REPEAT: 'repeat',
+
+  PLAYER_IMG_URI: 'deezer.com/images/cover/',
 };
 
 module.exports = constants;
