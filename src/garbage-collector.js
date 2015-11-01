3bc28c8a107dc581c737fbf9398ff94ac783bf5f
Bruno Queiros <brunoofq@gmail.com>
2015-11-01 18:38:42 -0200

    module to remove files from a folder

---
+++ src/garbage-collector.js
@@ -0,0 +1,9 @@
+'use strict';
+const fs = require('fs');
+const path = require('path');
+
+module.exports.clearFolder = function(folder) {
+  fs.readdirSync(folder).forEach((fileName) => {
+    fs.unlinkSync(path.join(folder, fileName));
+  });
+};
