c938cb98da9e37817c6146e3a26a20d13ceb26c4
Bruno Queiros <brunoofq@gmail.com>
2015-11-01 18:41:16 -0200

    method to udpate when a track update

--- src/player-helper.js
+++ src/player-helper.js
@@ -1,4 +1,5 @@
 'use strict';
+const imageDownloader = require('./image-downloader');
 
 var doc = null;
 var elements = {};
@@ -11,8 +12,10 @@ class PlayerHelper {
       'pause': doc.querySelector('.control-pause'),
       'next': doc.querySelector('.control-next'),
       'repeat': doc.querySelector('.control-repeat'),
-      'trackArtist': doc.querySelector('.player-track-link'),
-      'trackName': doc.querySelector('.player-track-link')
+      'track-artist': doc.querySelector('.player-track-title .player-track-link'),
+      'track-name': doc.querySelector('.player-track-artist .player-track-link'),
+      'track': doc.querySelector('.player-track'),
+      'track-cover': doc.querySelector('.player-cover img')
     };
   }
 
@@ -37,7 +40,31 @@ class PlayerHelper {
   }
 
   getCurrentMusic() {
-    return elements['trackArtist'] + ' ' + elements['trackName'];
+    return elements['track-artist'] + ' ' + elements['track-name'];
+  }
+
+  whenTrackChanged(callback) {
+    let track = {};
+
+    elements['track'].addEventListener('DOMSubtreeModified', (mutation) => {
+      if (mutation.target.nodeName === 'SPAN') {
+        track['track-name'] = mutation.target.innerText;
+      }
+
+      if (mutation.target.nodeName === 'H3') {
+        if (mutation.target.innerText.trim() !== 'by') {
+          track['track-artist'] = mutation.target.innerText.split('by')[1].trim();
+        }
+      }
+    });
+
+    elements['track-cover'].addEventListener('load', (event) => {
+      imageDownloader.download(event.path[0].src, (filename) => {
+        track['track-cover'] = filename;
+
+        callback(track);
+      });
+    });
   }
 }
 
